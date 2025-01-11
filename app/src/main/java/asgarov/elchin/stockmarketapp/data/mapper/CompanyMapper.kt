62831b66bdedede412e57072726b014a7ebb097c
AsgarovElchin <elcinaskerovki@gmail.com>
2025-01-11 18:31:15 +0400

    CompanyInfoDto mapper

--- app/src/main/java/asgarov/elchin/stockmarketapp/data/mapper/CompanyMapper.kt
+++ app/src/main/java/asgarov/elchin/stockmarketapp/data/mapper/CompanyMapper.kt
@@ -1,6 +1,8 @@
 package asgarov.elchin.stockmarketapp.data.mapper
 
 import asgarov.elchin.stockmarketapp.data.local.CompanyListingEntity
+import asgarov.elchin.stockmarketapp.data.remote.dto.CompanyInfoDto
+import asgarov.elchin.stockmarketapp.domain.model.CompanyInfo
 import asgarov.elchin.stockmarketapp.domain.model.CompanyListing
 
 fun CompanyListingEntity.toCompanyListing():CompanyListing{
@@ -17,4 +19,16 @@ fun CompanyListing.toCompanyListingEntity():CompanyListingEntity{
         symbol = symbol,
         exchange = exchange
     )
+}
+
+fun CompanyInfoDto.toCompanyInfo(): CompanyInfo {
+    return CompanyInfo(
+        symbol = symbol ?: "",
+        description = description ?: "",
+        name = name ?: "",
+        country = country ?: "",
+        industry = industry ?: ""
+
+    )
+
 }
