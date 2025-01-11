41f81b522c001bcff41716bc37cb5db06000d546
AsgarovElchin <elcinaskerovki@gmail.com>
2025-01-11 18:18:04 +0400

    add CompanyInfoDto and CompanyInfo classes

--- app/src/main/java/asgarov/elchin/stockmarketapp/presentation/company_listings/CompanyItem.kt
+++ app/src/main/java/asgarov/elchin/stockmarketapp/presentation/company_listings/CompanyItem.kt
@@ -8,9 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -36,6 +34,7 @@ fun CompanyItem(
             Spacer(modifier = Modifier.height(8.dp))
             Text(text = "(${company.symbol})",
                 fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
+                fontWeight = FontWeight.Bold,
                 color = MaterialTheme.colorScheme.onBackground)
     }
 }
